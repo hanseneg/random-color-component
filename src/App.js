9644f96697352a590bc9a7a6525852ebfae89de0
Ethan Hansen <hanseneg@hotmail.com>
2020-11-16 14:08:55 -0700

    commit2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react'
+const axios = require('axios')
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            color: ''
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://www.colr.org/json/color/random').then(response => {
+            this.setState({
+                color: response.data.new_color
+            })
+        })
+    }
+
     render() {
+
+        const color = {
+            backgroundColor: '#' + this.state.color,
+            height: '300px',
+            width: '500px',
+            fontFamily: 'sans-serif',
+            fontSize: '70px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'white',
+            
+        }
         return (
-            <div>
-                
+            <div style={color}>
+                Color Component
             </div>
         )
     }
